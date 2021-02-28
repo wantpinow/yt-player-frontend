5e61a35d67ed2419725fac650754305a84f3ab8b
pfrenett <patrick@frenett.net>
2021-02-28 00:18:57 +0000

    Create YoutubePlayer.vue

---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,17 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="url"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: "YoutubePlayer",
+  props: ["url"],
+};
+</script>
